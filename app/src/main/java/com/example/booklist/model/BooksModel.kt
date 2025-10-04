81dbc290d0279c72309c3fd0aeb6b5171c4ca5ce
Kauane Silva <kauane.nep@gmail.com>
2025-10-04 10:54:37 -0300

    Create object books

---
+++ app/src/main/java/com/example/booklist/model/BooksModel.kt
@@ -0,0 +1,9 @@
+package com.example.booklist.model
+
+data class BooksModel(
+    val name: String,
+    val image: String,
+    val description: String,
+    val score: String,
+    val author: String,
+)
