b5fa34b3c2fa2240ae6ec6bdab35c953574c8340
Kauane Silva <kauane.nep@gmail.com>
2025-10-08 16:24:50 -0300

    Refactor init info books

--- app/src/main/java/com/example/booklist/ui/theme/screens/ListBooks.kt
+++ app/src/main/java/com/example/booklist/ui/theme/screens/ListBooks.kt
@@ -3,28 +3,22 @@ package com.example.booklist.ui.theme.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.booklist.model.BooksModel
+import com.example.booklist.ui.theme.viewModel.BooksViewModel
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
-fun DashListBooks() {
-
-    val mockList: List<BooksModel> = List(3) {
-        BooksModel(
-            "livro teste",
-            "",
-            "Descriçao teste",
-            "5",
-            "Author Teste"
-        )
-    }
+fun DashListBooks(viewModel: BooksViewModel = koinViewModel()) {
+    LaunchedEffect(Unit) { viewModel.initVieModel() }
+    val listBooks = viewModel.stateValueBook.collectAsState().value
 
     Column(modifier = Modifier
         .padding(16.dp)
@@ -32,7 +26,7 @@ fun DashListBooks() {
         .verticalScroll(rememberScrollState())
     ) {
         LazyColumn(Modifier.weight(1f)) {
-            items(mockList) {
+            items(listBooks) {
                 ListItemDash(it)
             }
         }
